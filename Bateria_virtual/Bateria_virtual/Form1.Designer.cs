18e9abd6302c76ecd9d00601aca5180a687f83bb
Gregster <gustavo.gregorios@gmail.com>
2013-11-22 14:23:10 -0200

    Bateria virtual, feita com c#

---
+++ Bateria_virtual/Bateria_virtual/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Bateria_virtual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DiscardNull = true;
+            this.serialPort1.PortName = "COM3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Desconectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(158, 140);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(170, 156);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Batuino";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        public System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Bateria_virtual/Bateria_virtual/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using IrrKlang;
+using System.IO.Ports;
+
+
+
+namespace Bateria_virtual
+{
+    public partial class Form1 : Form
+    {
+        public string linha;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.DataReceived += serialPort1_DataReceived;
+            FormClosing += new FormClosingEventHandler(Form_Fechar);
+        }
+
+        private delegate void LineReceivedEvent(string line);
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            linha = serialPort1.ReadLine();
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), linha);
+        }
+        private void LineReceived(string line)
+        {
+            // richTextBox1.Text = line;
+            switch (line)
+            {
+                case "1":
+                    ISoundEngine engine1 = new ISoundEngine();
+                    engine1.Play2D(@"C:\Users\Gregster\Documents\Visual Studio 2012\Projects\Bateria_virtual\Bateria_virtual\HiHat.wav");
+                    break;
+                case "2":
+                    ISoundEngine engine2 = new ISoundEngine();
+                    engine2.Play2D(@"C:\Users\Gregster\Documents\Visual Studio 2012\Projects\Bateria_virtual\Bateria_virtual\Snare.wav");
+                    break;
+                case "3":
+                    ISoundEngine engine3 = new ISoundEngine();
+                    engine3.Play2D(@"C:\Users\Gregster\Documents\Visual Studio 2012\Projects\Bateria_virtual\Bateria_virtual\Tom.wav");
+                    break;
+
+                case "4":
+                    ISoundEngine engine4 = new ISoundEngine();
+                    engine4.Play2D(@"C:\Users\Gregster\Documents\Visual Studio 2012\Projects\Bateria_virtual\Bateria_virtual\Crash.wav");
+                    break;
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Open();
+                    serialPort1.Write("a");
+                }
+                catch
+                {
+                    MessageBox.Show("Ocorreu um erro. Por favor, certifique-se de que a porta correta foi selecionada, e se o dispositivo foi plugado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Write("b");
+                    serialPort1.Close();
+                    
+                }
+                catch
+                {
+                    MessageBox.Show("Ocorreu um erro. Por favor, certifique-se de que a porta correta foi selecionada, e se o dispositivo foi plugado.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+        private void Form_Fechar(object sender, FormClosingEventArgs e)
+        {
+            DialogResult resposta = MessageBox.Show("Deseja mesmo encerrar este aplicativo?",
+                                            "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (resposta == DialogResult.Yes)
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Write("b");
+                    serialPort1.Close();
+                }
+                else
+                {
+                    serialPort1.Close();
+                }
+            }
+            if (resposta == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+
+        }
+
+    }
+}
+
